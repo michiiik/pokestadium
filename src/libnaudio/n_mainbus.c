3cf59916f82402791098b4550b2025794242c29a
Michael Keller <49305086+michiiik@users.noreply.github.com>
2026-09-13 21:12:59 +0900

    Use the public audio stream header

--- src/libnaudio/n_mainbus.c
+++ src/libnaudio/n_mainbus.c
@@ -18,7 +18,7 @@
  *====================================================================*/
 
 #include "n_synthInternals.h"
-#include "../45720.h"
+#include "../audio_stream.h"
 
 // This function appears to be different than the stock n_alMainBusPull.
 
