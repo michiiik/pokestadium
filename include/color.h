c33e4ee25201d8ed638123d4ffcfc58e1ce6654e
Michael Keller <49305086+michiiik@users.noreply.github.com>
2026-09-15 15:54:06 +0900

    Match model vertex update functions

--- include/color.h
+++ include/color.h
@@ -33,6 +33,10 @@ typedef union Color_RGBA8_u32 {
     u32 rgba;
 } Color_RGBA8_u32;
 
+typedef struct {
+    f32 r, g, b;
+} Color_RGBf;
+
 typedef union Color_RGBA8_s32 {
     struct {
         s8 r, g, b, a;
