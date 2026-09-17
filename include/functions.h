282ca8e95884a205961cccef87d45f0dc6c8c39d
Michael Keller <49305086+michiiik@users.noreply.github.com>
2026-09-17 10:00:29 +0900

    Port model matches and rename fabsf symbol

--- include/functions.h
+++ include/functions.h
@@ -43,7 +43,7 @@ void __ull_divremi(unsigned long long* div, unsigned long long* rem, unsigned lo
 long long __ll_mod(long long a0, long long a1);
 long long __ll_rshift(long long a0, long long a1);
 
-f32 abs_asm(f32);
+f32 fabsf(f32);
 
 f32 __cosf(f32);
 f32 __sinf(f32);
